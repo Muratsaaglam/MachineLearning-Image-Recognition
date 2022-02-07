ecd1314791ede65d5ecc6e674328d51973d95311
Muratsaaglam <muratsaaglam@outlook.com>
2022-02-07 14:15:44 +0300

    Initial Commit

---
+++ MachineLearning/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MachineLearning
+//
+//  Created by Murat Sağlam on 7.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
